2f59ffe05ea7d226ef1d3a4a32cf94186fd23f6a
grzesiekjegrzeska <82875120+grzesiekjegrzeska@users.noreply.github.com>
2021-04-20 12:07:44 +0200

    Add files via upload

---
+++ src/com/company/Main.java
@@ -0,0 +1,33 @@
+/*
+    Zgadnij Liczbę gra.
+    Skrypt Zrobiony przez: grzesiekjegrzeska
+    github: https://github.com/grzesiekjegrzeska
+ */
+package com.company;
+import java.util.Random;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        int i = 0;
+        int los;
+        int odp;
+        Random rnd = new Random();
+        los = rnd.nextInt(10) +1;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Skrypt Zrobiony przez: grzesiekjegrzeska | c:");
+        System.out.println(" ");
+        System.out.println("Zgadnij liczbe od 1 do 10");
+        do {
+            i++;
+            System.out.print("Podaj liczbe: ");
+            odp = scanner.nextInt();
+            if (odp > los) {
+                System.out.println("Nie zgadles! Moja liczba jest mniejsza.");
+            }
+            else if (odp < los) {
+                System.out.println("Nie zgadles! Moja liczba jest wieksza.");
+            }
+        } while (odp != los);
+        System.out.println("Brawo odgadles za " + i + " razem!");
+    }
+}
